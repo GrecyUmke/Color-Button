4c70d18c713b5c0e4b905c21a7132a0edfa2cdb6
Grecy Umke <103163841+GrecyUmke@users.noreply.github.com>
2023-06-27 11:37:23 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,19 @@
+const box=document.getElementById("box")
+
+const red_button =document.getElementById("red")
+red_button.onclick = function() {
+    box.style.backgroundColor = "red";
+}
+const yellow_button =document.getElementById("yellow")
+yellow_button.onclick = function() {
+    box.style.backgroundColor = "yellow";
+}
+const black_button =document.getElementById("black")
+black_button.onclick = function() {
+    box.style.backgroundColor = "black";
+}
+const purple_button =document.getElementById("purple")
+purple_button.onclick = function() {
+    box.style.backgroundColor = "purple";
+}
+
